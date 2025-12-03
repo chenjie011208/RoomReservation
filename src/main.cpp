f45436ce99f45d99bf901f1ed2581cd8ad320e7e
ChenJie <chenjie746955058@163.com>
2025-12-04 01:45:24 +0800

    add init code

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int select = 0; // 用于接受用户的选择
+    
+    while(true)
+    {
+        
+    
+    cout << "==================== 欢迎来到传智机房预约系统 ====================" << endl;
+    cout << endl << "请输入您的身份" << endl;
+    cout << "\t\t ----------------------------------------\n";
+    cout << "\t\t|                                        |\n";
+    cout << "\t\t|                1.学生代表              |\n";
+    cout << "\t\t|                                        |\n";
+    cout << "\t\t|                2.老    师              |\n";
+    cout << "\t\t|                                        |\n";
+    cout << "\t\t|                3.管 理 员              |\n";
+    cout << "\t\t|                                        |\n";
+    cout << "\t\t|                0.退    出              |\n";
+    cout << "\t\t|                                        |\n";
+    cout << "\t\t ----------------------------------------\n";
+    cout << "请输入您的选择：";
+
+    cin >> select; // 接受用户选择
+    
+    switch(select) // 根据用户选择 实现不同接口
+    {
+        case 1: // 学生身份
+	    break;
+	case 2: // 老师身份
+	    break;
+	case 3: // 管理员身份
+	    break;
+	case 0: // 退出系统
+	    break;
+	default:
+	    cout << "输入有误，请重新原则" << endl;
+	    cin.clear();
+	    cin.ignore(1024, '\n');
+	    system("bash -c \"read -n 1 -s -p '按任意键继续...'\"");
+            system("clear");
+	    break;
+    
+    
+    }
+
+
+    }
+    return 0;
+}
