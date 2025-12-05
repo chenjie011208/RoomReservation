c4e0417fb01147da83d707c91dfe4876007b1da1
ChenJie <chenjie746955058@163.com>
2025-12-06 02:38:04 +0800

    code update

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,55 @@ using namespace std;
 #include "globalFile.h"
 #include "student.h"
 #include "teacher.h"
+#include "manager.h"
+
+// 进入管理员子菜单界面
+void managerMenu(Identity* &manager)
+{
+    while (true)
+    {
+        // 调用管理员子菜单
+        manager->openMenu();
+
+        // 将父类的指针转为子类的指针,调用子类里其它接口
+        Manager * man = (Manager*)manager;
+
+        int select = 0;
+        // 接受用户的选择
+        cin >> select;
+
+        if (select == 1) // 添加账号
+        {
+            cout << "添加账号" << endl;
+            man->addPerson();
+        }
+        else if (select == 2) // 查看账号
+        {
+            cout << "查看账号" << endl;
+            man->showPerson();
+        }
+        else if (select == 3) // 查看机房
+        {
+            cout << "查看机房" << endl;
+            man->showComputer();
+        }
+        else if (select == 4) // 清空预约
+        {
+            cout << "清空预约" << endl;
+            man->clearFile();
+        }
+        else
+        {
+            delete manager; // 销毁掉堆区对象
+            cout << "注销成功" << endl;
+            cin.clear();
+            cin.ignore(1024, '\n');
+            system("bash -c \"read -n 1 -s -p '按任意键继续...'\"");
+            system("clear");
+            return;
+        }
+    }
+}
 
 // 登录功能 参数1 操作文件名 参数2 操作身份类型
 void LoginIn(string fileName, int type)
@@ -95,6 +144,22 @@ void LoginIn(string fileName, int type)
     else if (type == 3)
     {
         // 管理员身份验证
+        string fName;
+        string fPwd;
+        while (ifs >> fName && ifs >> fPwd)
+        {
+            if (name == fName && pwd == fPwd)
+            {
+                cout << "验证登录成功" << endl;
+                cin.clear();
+                cin.ignore(1024, '\n');
+                system("bash -c \"read -n 1 -s -p '按任意键继续...'\"");
+                system("clear");
+                person = new Manager(name, pwd);
+                managerMenu(person);
+                return;
+            }
+        }
     }   
 
     cout << "验证登录失败" << endl;
