69b3dfd86ba32966858f440db621cbe332e9547d
ChenJie <chenjie746955058@163.com>
2025-12-06 02:03:56 +0800

    student login in success

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,92 @@
 #include <iostream>
 using namespace std;
+#include "identity.h"
+#include <fstream>
+#include <string>
+#include "globalFile.h"
+#include "student.h"
+
+// 登录功能 参数1 操作文件名 参数2 操作身份类型
+void LoginIn(string fileName, int type)
+{
+    // 父类指针， 用于指向子类对象
+    Identity* person = NULL;
+
+    // 读文件
+    ifstream ifs;
+    ifs.open(fileName, ios::in);
+
+    // 判断文件是否存在
+    if (!ifs.is_open())
+    {
+        cout << "文件不存在" << endl;
+        ifs.close();
+        return;
+    }
+
+    // 准备接受用户的信息
+    int id = 0;
+    string name;
+    string pwd;
+
+    // 判断身份
+    if (type == 1) // 学生身份
+    {
+        cout << "请输入你的学号" << endl;
+        cin >> id;
+    }
+    else if (type == 2) // 老师身份
+    {
+        cout << "请输入您的职工号" << endl;
+        cin >> id;
+    }
+
+    cout << "请输入用户名" << endl;
+    cin >> name;
+
+    cout << "请输入密码" << endl;
+    cin >> pwd;
+
+    if (type == 1)
+    {
+        // 学生身份验证
+        int fId; // 从文件中获取的id号
+        string fName; // 从文件中获取的姓名
+        string fPwd; // 从文件中获取的密码
+        while (ifs >> fId && ifs >> fName && ifs >> fPwd)
+        {
+            if (id == fId && name == fName && pwd == fPwd) // 与用户输入信息做对比
+            {
+                cout << "学生验证登录成功" << endl;
+                cin.clear();
+                cin.ignore(1024, '\n');
+                system("bash -c \"read -n 1 -s -p '按任意键继续...'\"");
+                system("clear");
+                person = new Student(id, name, pwd);
+
+                return;
+            }
+
+        }
+    }
+    else if (type == 2)
+    {
+        // 教师身份验证
+    }
+    else if (type == 3)
+    {
+        // 管理员身份验证
+    }   
+
+    cout << "验证登录失败" << endl;
+    cin.clear();
+    cin.ignore(1024, '\n');
+    system("bash -c \"read -n 1 -s -p '按任意键继续...'\"");
+    system("clear");
+
+    return;
+}
+
 
 int main()
 {
@@ -28,11 +115,14 @@ int main()
     
     switch(select) // 根据用户选择 实现不同接口
     {
-        case 1: // 学生身份
+    case 1: // 学生身份
+        LoginIn(STUDENT_FILE, 1);
 	    break;
 	case 2: // 老师身份
+        LoginIn(TEACHER_FILE, 2);
 	    break;
 	case 3: // 管理员身份
+        LoginIn(ADMIN_FILE, 3);
 	    break;
 	case 0: // 退出系统
 	    cout << "欢迎下一次使用" << endl;
